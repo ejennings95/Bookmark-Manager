2252e06e8afcc34aa82119288ad83d32080a9b93
Elliot Jennings <elliot.r.jennings@outlook.com>
2019-01-29 13:18:01 +0000

    refactored and fix printing bug

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -2,19 +2,14 @@ require 'PG'
 
 class Bookmark
 
-  @@list = []
-
   def initialize(link)
     @link = link
   end
 
   def self.list
     conn = PG.connect( dbname: 'bookmark_manager' )
-    conn.exec( "SELECT * FROM bookmarks" ) do |result|
-      result.each do |row|
-        @@list.push(row.values_at('url'))
-      end
-      @@list
+    conn.exec( "SELECT * FROM bookmarks" ).map do | row |
+      row['url']
     end
   end
 end
